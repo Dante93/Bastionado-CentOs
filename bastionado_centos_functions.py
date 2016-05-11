88872ba26c1aaa97f530c59a1f7be5803253a494
Dante93 <kevin.borras.soler@gmail.com>
2016-05-11 12:04:43 +0200

    modificando los writes

--- bastionado_centos_functions.py
+++ bastionado_centos_functions.py
@@ -18,13 +18,13 @@ def cramfs_filesys():
 	if (res == False || res2 == False):
 
 		if os.path.exists('/etc/modprobe.d/CIS.conf'):
-			aux = open('/etc/modprobe.d/CIS.conf',r)
-			aux.write("install cramfs /bin/true")
+			aux = open('/etc/modprobe.d/CIS.conf','w')
+			aux.write("install cramfs /bin/true \n")
 		
 
 		else:
-			aux = open('/etc/modprobe.d/CIS.conf',w)
-			aux.write("install cramfs /bin/true")
+			aux = open('/etc/modprobe.d/CIS.conf','a')
+			aux.write("install cramfs /bin/true \n")
 			aux.close()
 
 	else:
@@ -42,12 +42,12 @@ def freevcfs_disble():
 
 	if (res == False || res2 == False):
 		if os.path.exists('/etc/modprobe.d/CIS.conf'):
-			aux = open('/etc/modprobe.d/CIS.conf',r)
+			aux = open('/etc/modprobe.d/CIS.conf \n','w')
 			aux.write("install freevxfs /bin/true")
 
 		else:
-			aux = open('/etc/modprobe.d/CIS.conf',w)
-			aux.write("install freevxfs /bin/true")
+			aux = open('/etc/modprobe.d/CIS.conf','a')
+			aux.write("install freevxfs /bin/true \n")
 			aux.close()
 
 	else:
@@ -65,12 +65,12 @@ def jffs2_disable():
 
 	if (res == False || res2 == False):
 		if os.path.exists('/etc/modprobe.d/CIS.conf'):
-			aux = open('/etc/modprobe.d/CIS.conf',r)
-			aux.write("install freevxfs /bin/true")
+			aux = open('/etc/modprobe.d/CIS.conf \n','w')
+			aux.write("install jffs2 /bin/true")
 
 		else:
-			aux = open('/etc/modprobe.d/CIS.conf',w)
-			aux.write("install jffs2 /bin/true")
+			aux = open('/etc/modprobe.d/CIS.conf','a')
+			aux.write("install jffs2 /bin/true \n")
 			aux.close()
 
 	else:
